9c1dbd33a2071c22a24547bec0ca9cbb06a55018
ItzStoopy <93840089+ItzStoopy@users.noreply.github.com>
2022-03-19 19:39:57 -0700

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function go()
+{
+
+play1= document.getElementById("input1").value;
+play2= document.getElementById("input2").value;
+localStorage.setItem("text1", play1);
+localStorage.setItem("text2", play2);
+window.location=""
+
+}
